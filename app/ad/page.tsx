f5d98b78975e9a580f60c106330bc0fd68aeb8ea
Chaibouu <chaibouabdoulwahab@gmail.com>
2024-02-01 00:39:36 +0100

    add ad

--- app/ad/page.tsx
+++ app/ad/page.tsx
@@ -5,9 +5,10 @@ const page = () => {
     return (
         <>
             <Navbar />
-            <div>
-                <h1>Ajouter des ficher</h1>
-                <form action="">
+           <div className='flex items-center justify-center '>
+             <div className='lg:w-1/2 m-10 border'>
+                <h1 className='text-center md:text-3xl text-2xl bg-slate-700 py-4 text-white'>Ajouter des ficher</h1>
+                <form action="" className='flex flex-col md:p-10 p-4'>
                     <label htmlFor="">Nom du film</label>
                     <input className="nomFilm" name="nomFilm" type="text" placeholder="Nom du Film"/>
                     <label htmlFor="">type</label>
@@ -19,13 +20,14 @@ const page = () => {
                     <label htmlFor="">Saison</label>
                     <input className="saisonFilm" name="saisonFilm" type="text" placeholder="Entrer les Saison"/>
                     <label htmlFor="">Image</label>
-                    <input className="imageFilm"  name="imageFilm" type="file" placeholder="Image du film"/>
-                    <div>
-                        <button type="submit" id="btnFilm" className="btnFilm">Ajouter</button>
+                    <input className="border "  name="imageFilm" type="file" placeholder="Image du film"/>
+                    <div className='w-full flex items-center justify-center m-4'>
+                        <button type="submit" id="btnFilm" className="border p-2 px-8 bg-blue-700 rounded">Ajouter</button>
                     </div>
                     {/* comment */}
                 </form>
             </div>
+           </div>
         </>
     );
 };
