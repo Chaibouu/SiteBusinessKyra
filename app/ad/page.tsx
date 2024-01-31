ff887097c78cb4db59830e2350bc17bf9065ea43
Chaibouu <chaibouabdoulwahab@gmail.com>
2024-02-01 00:20:00 +0100

    add

--- app/ad/page.tsx
+++ app/ad/page.tsx
@@ -9,34 +9,25 @@ const page = () => {
                 <h1>Ajouter des ficher</h1>
                 <form action="">
                     <label htmlFor="">Nom du film</label>
-                    <input className="nomFilm" name="nomFilm" type="text" placeholder="Nom du Film">
+                    <input className="nomFilm" name="nomFilm" type="text" placeholder="Nom du Film"/>
                     <label htmlFor="">type</label>
                     <select className="typeFilm" name="typeFilm">
-                    <option value="Tout les Catégorie">Tout les Catégorie</option>
-                    <option value="Film">Film</option>
-                    <option value="Série">Série</option>
+                        <option value="Tout les Catégorie">Tout les Catégorie</option>
+                        <option value="Film">Film</option>
+                        <option value="Série">Série</option>
                     </select>
                     <label htmlFor="">Saison</label>
-                    <input className="saisonFilm" name="saisonFilm" type="text" placeholder="Entrer les Saison">
+                    <input className="saisonFilm" name="saisonFilm" type="text" placeholder="Entrer les Saison"/>
                     <label htmlFor="">Image</label>
-                    <input className="imageFilm"  name="imageFilm" type="file" placeholder="Image du film">
+                    <input className="imageFilm"  name="imageFilm" type="file" placeholder="Image du film"/>
                     <div>
-                    <button type="submit" id="btnFilm" className="btnFilm">Ajouter</button>
+                        <button type="submit" id="btnFilm" className="btnFilm">Ajouter</button>
                     </div>
                 </form>
             </div>
-            {/* comment */}
-            {/* comment */}
-            {/* comment */}
-            {/* comment */}
-            {/* comment */}
-            {/* comment */}
-            {/* comment */}
-            {/* comment */}
-            {/* comment */}
-            {/* comment */}
         </>
     );
 };
+
 
 export default page;
