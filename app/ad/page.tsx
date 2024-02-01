5ea238b8feb8c2f2309ec0ee3477b08fe0e12aec
Chaibouu <chaibouabdoulwahab@gmail.com>
2024-02-02 00:14:16 +0100

    add

--- app/ad/page.tsx
+++ app/ad/page.tsx
@@ -37,6 +37,7 @@ const page = () => {
                     {/* comment */}
                     {/* comment */}
                     {/* comment */}
+                    {/* comment */}
                 </form>
             </div>
            </div>
